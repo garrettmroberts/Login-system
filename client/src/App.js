7dbaefb416201998cc7c16b3463bc162fee357b1
Garrett Roberts <garrettmroberts@gmail.com>
2020-06-30 12:22:32 -0500

    integrated bootstrap 5 + scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './css/main.css';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className='App container-fluid'>
+    <div className='d-flex bg-secondary' id='app'>
       <Login />
     </div>
     
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import './style.css';
+import Lock from '../../../node_modules/bootstrap-icons/icons/lock.svg';
+import '../../css/main.css';
+
 
 const Login = () => {
   return(
-    <div className='row d-flex justify-content-center align-items-center' id='loginDiv'>
-      <div className='card' id='loginCard'>
-        <form>
-          <h2>Login</h2>
-          <div className='form-group'>
-            <label htmlFor='email'>Email: </label>
-            <input type='email' className='form-control' id='email' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Email: </label>
-            <input type='password' className='form-control' id='password' />
-          </div>
-          <button type='submit' class='btn btn-primary'>Submit</button>
-        </form>
-      </div>
-    </div>
+    <form id='form-login'>
+      <img src={Lock} id='loginLock' className='mb-4' />
+      <h1 className='h3 mb-3 font-weight-normal'>Login</h1>
+      <label for='inputEmail' className='sr-only'>Email Address</label>
+      <input 
+        type='email'
+        id='inputEmail'
+        className='form-control mb-2'
+        placeholder='Email address'
+        required
+      />
+      <label for='inputPassword' className='sr-only'>Password</label>
+      <input
+        type='password'
+        id='inputPassword'
+        className='form-control mb-2'
+        placeholder='password'
+        required
+      />
+      <button class='btn btn-lg btn-primary btn-block' type='submit'>
+        Login
+      </button>
+    </form>
   )
 };
 

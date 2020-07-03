6cabe370f8e89334b136154c989060a483d5e875
Garrett Roberts <garrettmroberts@gmail.com>
2020-07-03 08:48:45 -0500

    heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
+const Nothing = () => {
+  return(
+    <h1>Hello</h1>
+  )
+}
+
 function App() {
   return (
     <Router>
       <div className='d-flex bg-secondary' id='app'>
         <Switch>
+          <Route exact path='/' component={Nothing} />
           <Route exact path='/login' component={Login} />
           <Route exact path = '/signup' component={Signup} />
         </Switch>
